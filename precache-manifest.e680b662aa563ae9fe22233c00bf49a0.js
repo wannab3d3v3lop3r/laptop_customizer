cf9c65fa85e79ff925188847e1039cec687d71a5
wannab3d3v3lop3r <wannab3d3v3lop3r@gmail.com>
2019-09-19 22:41:00 -0400

    Updates

---
+++ precache-manifest.e680b662aa563ae9fe22233c00bf49a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86fad1fc3e2d20965f93",
+    "url": "/laptop_customizer/static/js/runtime~main.86fad1fc.js"
+  },
+  {
+    "revision": "86e200d1734ce5b4575f",
+    "url": "/laptop_customizer/static/js/main.86e200d1.chunk.js"
+  },
+  {
+    "revision": "26997e4f8439735a840d",
+    "url": "/laptop_customizer/static/js/1.26997e4f.chunk.js"
+  },
+  {
+    "revision": "86e200d1734ce5b4575f",
+    "url": "/laptop_customizer/static/css/main.fe812ab5.chunk.css"
+  },
+  {
+    "revision": "26ab1740d0e30593fa5615e445f7f17a",
+    "url": "/laptop_customizer/index.html"
+  }
+];
